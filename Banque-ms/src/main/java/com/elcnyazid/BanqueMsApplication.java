2907b82391b61259b23bff0d1fcfd0931f9195db
Yazid Elcouhen <elcouhenyazid@gmail.com>
2021-10-16 16:25:34 +0100

    creating the project

---
+++ Banque-ms/src/main/java/com/elcnyazid/BanqueMsApplication.java
@@ -0,0 +1,13 @@
+package com.elcnyazid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BanqueMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BanqueMsApplication.class, args);
+	}
+
+}
