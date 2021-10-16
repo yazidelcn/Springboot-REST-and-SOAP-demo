276c7b42d5b3bc03b5955746f509f759f5e5c12c
Yazid Elcouhen <elcouhenyazid@gmail.com>
2021-10-16 16:45:45 +0100

    configuring and seeding the h2 db

--- Banque-ms/src/main/java/com/elcnyazid/banque/BanqueMsApplication.java
+++ Banque-ms/src/main/java/com/elcnyazid/banque/BanqueMsApplication.java
@@ -1,7 +1,14 @@
 package com.elcnyazid.banque;
 
+import com.elcnyazid.banque.entities.Compte;
+import com.elcnyazid.banque.entities.Type;
+import com.elcnyazid.banque.repositories.CompteRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Date;
 
 @SpringBootApplication
 public class BanqueMsApplication {
@@ -10,4 +17,16 @@ public class BanqueMsApplication {
 		SpringApplication.run(BanqueMsApplication.class, args);
 	}
 
+
+	@Bean
+	CommandLineRunner start(CompteRepository compteRepository){
+		return args -> {
+			compteRepository.save(new Compte(null, Math.random()*9000, new Date(), Type.EPARGNE));
+			compteRepository.save(new Compte(null, Math.random()*9000, new Date(), Type.EPARGNE));
+			compteRepository.save(new Compte(null, Math.random()*9000, new Date(), Type.COURANT));
+			compteRepository.findAll().forEach(c ->{
+				System.out.println(c	);
+			});
+		};
+	}
 }
